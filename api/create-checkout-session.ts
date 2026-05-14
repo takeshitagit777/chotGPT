4f2248f061b2ceaa58f9cac0d7122ee9cb48060d
takeshitagit777 <153724752+takeshitagit777@users.noreply.github.com>
2026-05-15 00:33:57 +0900

    Update create-checkout-session.ts

--- api/create-checkout-session.ts
+++ api/create-checkout-session.ts
@@ -1,7 +1,5 @@
 import Stripe from "stripe";
-
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
-
 export default async function handler(req: any, res: any) {
   if (req.method !== "POST") {
     return res.status(405).json({
